0eb78bc9c0db401da3f08abeb590567ed080d034
Provotorov-A-A <39033373+Provotorov-A-A@users.noreply.github.com>
2021-02-01 15:28:23 +0300

    Create basics.tcl

---
+++ tcl/basics.tcl
@@ -0,0 +1,7 @@
+puts "Tcl source code is sequentials of command separated by semicolon or newline".
+puts "Command may have parameters, separated by spaces."
+puts "If parameter contains spaces it should be bordered."
+puts "Parameter may be bourdered in 2 ways:"
+puts "First: with a quotes."
+puts "Second: with a curly brackets." 
+puts "Using quotes do may possible command substitutions inside parameters, while curly brackets prevents any substitutions - parameter will pass "as is"."
